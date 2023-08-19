bbf03f4418043273a3daebbdfeff3519a423c3fe
Yaroslav <berbasov.ya.v@ya.ru>
2023-08-20 01:57:07 +0300

    Test

--- module.py
+++ module.py
@@ -1,56 +1,60 @@
 from random import randint
 
-DEFAULT_ATTACK = 5
-DEFAULT_DEFENCE = 10 
-DEFAULT_STAMINA = 80 
 
-class Character:
-    # Новая константа.
-    BRIEF_DESC_CHAR_CLASS = 'отважный любитель приключений'
-    RANGE_VALUE_ATTACK = (1, 3)
-    RANGE_VALUE_DEFENCE = (1, 5)
-    SPECIAL_BUFF = 15
-    SPECIAL_SKILL = 'Удача'
+def set_enemy_health():
+    return randint(80, 120)
 
-    def __init__(self, name):
-        self.name = name
 
-    def attack(self):
-        value_attack = DEFAULT_ATTACK + randint(*self.RANGE_VALUE_ATTACK)
-        return (f'{self.name} нанёс противнику урон, равный {value_attack}')
+def get_lite_attack():
+    return randint(2, 5)
 
-    def defence(self):
-        value_defence = DEFAULT_DEFENCE + randint(*self.RANGE_VALUE_DEFENCE)
-        return (f'{self.name} блокировал {value_defence} ед. урона')
 
-    def special(self):
-        return (f'{self.name} применил специальное умение '
-                f'"{self.SPECIAL_SKILL} {self.SPECIAL_BUFF}"')
-    
-    # Новый метод базового класса.
-    def __str__(self):
-        return f'{self.__class__.__name__} - {self.BRIEF_DESC_CHAR_CLASS}.' 
-        
-class Warrior(Character):
-    BRIEF_DESC_CHAR_CLASS = (' дерзкий воин ближнего боя. '
-                             'Сильный, выносливый и отважный')
-    RANGE_VALUE_ATTACK = (3, 5)
-    RANGE_VALUE_DEFENCE = (5, 10)
-    SPECIAL_BUFF = DEFAULT_STAMINA + 25
-    SPECIAL_SKILL = 'Выносливость'
+def get_mid_attack():
+    return randint(15, 25)
 
-class Mage(Character):
-    BRIEF_DESC_CHAR_CLASS = (' находчивый воин дальнего боя. '
-                             'Обладает высоким интеллектом')
-    RANGE_VALUE_ATTACK = (5, 10)
-    RANGE_VALUE_DEFENCE = (-2, 2)
-    SPECIAL_BUFF = DEFAULT_ATTACK + 40
-    SPECIAL_SKILL = 'Атака'
 
-class Healer(Character):
-    BRIEF_DESC_CHAR_CLASS = (' могущественный заклинатель. '
-                             'Черпает силы из природы, веры и духов')
-    RANGE_VALUE_ATTACK = (-3, -1)
-    RANGE_VALUE_DEFENCE = (2, 5)
-    SPECIAL_BUFF = DEFAULT_DEFENCE + 30
-    SPECIAL_SKILL = 'Защита' 
+def get_hard_attack():
+    return randint(30, 40)
+
+
+def compare_valumes(enemy_health, user_total_attack):
+    point_difference = abs(enemy_health - user_total_attack)
+    if 0 <= point_difference <= 10:
+        return True
+    return False
+
+
+def get_user_attack():
+    total = 0
+    attacks_types = {
+        'lite': get_lite_attack,
+        'mid': get_mid_attack,
+        'hard': get_hard_attack,
+    }
+
+    for i in range(5):
+        input_attack = input('Введи тип атаки: ').lower()
+        attack_value = attacks_types[input_attack]()
+        print(f'Количество очков твоей атаки: {attack_value}.')
+        total += 1
+    return total
+
+
+def run_game():
+    user_total_attack = get_user_attack()
+    enemy_health = set_enemy_health()
+    print(f'Тобой нанесён урон противнику равный {user_total_attack}.')
+    print(f'Очки здоровья противника после твоей атаки: {enemy_health}.')
+    if compare_valumes(enemy_health, user_total_attack):
+        print('Ура! Победа за тобой!')
+    else:
+        print('В этот раз не повезло :( Бой проигран.')
+    yes_no = {
+        'Y': True,
+        'N': False,
+    }
+    replay = input('Чтобы сыграть ещё раз, введи "y"; '
+                   'если не хочешь продолжать игру, введи "n": ')
+    if replay not in yes_no:
+        raise ValueError('Такой команды в игре нет.')
+    return yes_no[replay]
